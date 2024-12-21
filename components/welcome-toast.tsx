5fc7fbeeaa3db48b6b028552876289170667592c
AlexanderSergan <veteroque@gmail.com>
2024-12-21 18:25:43 +0200

    welcome toast;

--- components/welcome-toast.tsx
+++ components/welcome-toast.tsx
@@ -8,7 +8,7 @@ export function WelcomeToast() {
     // ignore if screen height is too small
     if (window.innerHeight < 650) return;
     if (!document.cookie.includes('welcome-toast=2')) {
-      toast('🛍️ Welcome to Next.js Commerce!', {
+      toast('🛍️ Welcome to F-Players', {
         id: 'welcome-toast',
         duration: Infinity,
         onDismiss: () => {
@@ -16,15 +16,15 @@ export function WelcomeToast() {
         },
         description: (
           <>
-            This is a high-performance, SSR storefront powered by Shopify, Next.js, and Vercel.{' '}
-            <a
+            Navigate around and you will find socials of any player you like. ⚽️{' '}
+            {/* <a
               href="https://vercel.com/templates/next.js/nextjs-commerce"
               className="text-blue-600 hover:underline"
               target="_blank"
             >
               Deploy your own
-            </a>
-            .
+            </a> */}
+            {/* . */}
           </>
         )
       });
